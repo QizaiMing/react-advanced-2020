fc25f57221dbc5e483767334bfc71576c8f43cdc
Juan Chacon <mahnimex@gmail.com>
2020-11-15 18:27:57 -0400

    useState and useEffect

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  // const [person, setPerson] = useState({
+  //   name: 'peter',
+  //   age: 24,
+  //   message: 'random message'
+  // })
 
-export default UseStateObject;
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('hello world')
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hello world' })
+    setMessage('hello')
+  }
+
+  const changeName = () => setName('juanito')
+
+  const changeAge = () => setAge(20)
+
+  return (
+    <React.Fragment>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>
+        {message} {name}
+      </h3>
+      <button className='btn' onClick={changeName}>
+        change name
+      </button>
+      <button className='btn' onClick={changeAge}>
+        change age
+      </button>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateObject
