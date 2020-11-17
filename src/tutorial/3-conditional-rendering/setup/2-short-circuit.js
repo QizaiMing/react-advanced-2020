021066338a9fb0c0c284401498da9fa86afe6d20
Juan Chacon <mahnimex@gmail.com>
2020-11-17 00:15:25 -0400

    Ternary operator and show/hide component

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
+// condition ? "executes if true" : "executes if false"
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
+  const [error, setError] = useState(false)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
 
   return (
     <React.Fragment>
       {/* <h1>{firstValue}</h1>
       <h1>value: {secondValue}</h1> */}
       <h1>{text || 'John Doe'}</h1>
-      {text && <h1>hello world</h1>}
+      <button className='btn' onClick={() => setError(!error)}>
+        toggle error
+      </button>
+      {error && <h1>Error...</h1>}
+      {error ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no error...</h2>
+        </div>
+      )}
     </React.Fragment>
   )
 }
